a271fdf6229e5a0cfd6278000382a0f99ba19836
Will Daughtridge <wdaughtridge@gmail.com>
2021-10-01 11:00:47 -0400

    Initial Commit

---
+++ Trashi/TrashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrashiApp.swift
+//  Trashi
+//
+//  Created by William Daughtridge on 10/1/21.
+//
+
+import SwiftUI
+
+@main
+struct TrashiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
