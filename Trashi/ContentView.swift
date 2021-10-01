a2a734b0c34880f7003eda07315eaeacedef888f
Will Daughtridge <wdaughtridge@gmail.com>
2021-10-01 14:50:36 -0400

    dark mode edit + upc compat

--- Trashi/ContentView.swift
+++ Trashi/ContentView.swift
@@ -6,11 +6,55 @@
 //
 
 import SwiftUI
+import CodeScanner
 
 struct ContentView: View {
+    @Environment(\.colorScheme) var colorScheme
+    @State var isPresentingScanner = false
+    @State var scannedCode: String?
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            if (colorScheme == .dark) {
+                Image("Logo - dark")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.horizontal, 100.0)
+            } else {
+                Image("Logo")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.horizontal, 100.0)
+            }
+            
+            Button {
+                self.isPresentingScanner = true
+            } label: {
+                Text("Scan Barcode")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding()
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                            .background(Color(red: 43/255, green: 74/255, blue: 52/255))
+                            .cornerRadius(40.0)
+            }
+            .padding(.horizontal, 40.0)
+            .sheet(isPresented: $isPresentingScanner) {
+                self.scannerSheet
+            }
+        }
+    }
+
+    var scannerSheet : some View {
+        CodeScannerView(
+            codeTypes: [.code39, .code128, .code93, .upce, .code39Mod43, .ean13, .ean8],
+            completion: { result in
+                if case let .success(code) = result {
+                    self.scannedCode = code
+                    self.isPresentingScanner = false
+                }
+            }
+        )
     }
 }
 
